90b2dccf9d1af8589e5c1c3076a352599a83a5ee
benjv_tdh <cortesbenja@gmail.com>
2022-06-14 22:40:38 -0400

    displaying salida_detail.html

--- salida/urls.py
+++ salida/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from salida.views import SalidaCreateView, SalidaListView
-
+from salida.views import (  SalidaCreateView, SalidaListView ,
+                            SalidaDetailView)
 
 app_name = 'salida'
 urlpatterns = [
     path('create', SalidaCreateView.as_view(), name='salida_form'),
     path('', SalidaListView.as_view(), name='salida_list'),
+    path('detail/<int:pk>',SalidaDetailView.as_view(),name='salida_detail') ,
 ]
--- salida/views.py
+++ salida/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView
 from django.urls import reverse_lazy
 from salida.models import Salida
 
-# Create your views here.
-
 class SalidaCreateView(CreateView):
+    # model_form.html --> salida_form.html
     model = Salida
     fields = '__all__'
     success_url = reverse_lazy('home:home')
 
 
 class SalidaListView(ListView):
+
+    ## model_list.html --> salida_list.html
     model = Salida
     context_object_name = 'salida_list'
+
+class SalidaDetailView(DetailView):
+
+    # model_detial.html --> salida_detail.html
+    model = Salida
