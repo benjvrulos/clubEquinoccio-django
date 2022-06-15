6840f10cffe5bf83a0759545ab94b3f39e81a8c8
benjv_tdh <cortesbenja@gmail.com>
2022-06-15 01:15:09 -0400

    creating SalidaForm

--- salida/urls.py
+++ salida/urls.py
@@ -6,7 +6,7 @@ from salida.views import (  SalidaCreateView,SalidaDeleteView,
 app_name = 'salida'
 urlpatterns = [
     path('', SalidaListView.as_view(), name='salida-list'),
-    path('create', SalidaCreateView.as_view(), name='salida-form'),
+    path('create/', SalidaCreateView.as_view(), name='salida-form'),
     path('detail/<int:pk>', SalidaDetailView.as_view(), name='salida-detail') ,
     path('update/<int:pk>', SalidaUpdateView.as_view(), name='salida-update'),
     path('delete/<int:pk>', SalidaDeleteView.as_view(), name='salida-delete'),
--- salida/views.py
+++ salida/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView, UpdateView , DeleteView
 from django.urls import reverse_lazy
 from salida.models import Salida
+from salida.forms import SalidaForm
 
 class SalidaCreateView(CreateView):
     # model_form.html --> salida_form.html
     model = Salida
-    fields = '__all__'
+    # fields = '__all__'
     success_url = reverse_lazy('home:home')
+    context_object_name = 'salida'
+    form_class = SalidaForm
 
 
 class SalidaListView(ListView):
