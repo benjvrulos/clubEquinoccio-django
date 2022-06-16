9b8da7d1f63542c5a38b310a1f4a356194469283
benjv_tdh <cortesbenja@gmail.com>
2022-06-16 00:41:47 -0400

    salida-form.hmtl with fancy template

--- salida/views.py
+++ salida/views.py
@@ -7,10 +7,10 @@ from salida.forms import SalidaForm
 class SalidaCreateView(CreateView):
     # model_form.html --> salida_form.html
     model = Salida
-    # fields = '__all__'
+    fields = '__all__'
     success_url = reverse_lazy('home:home')
     context_object_name = 'salida'
-    form_class = SalidaForm
+    # form_class = SalidaForm
 
 
 class SalidaListView(ListView):
