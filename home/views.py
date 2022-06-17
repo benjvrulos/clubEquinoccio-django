f02208ac9c091e62233dd266136bb2c47ddbc95c
benjv_tdh <cortesbenja@gmail.com>
2022-06-17 02:15:02 -0400

    displaying salida photos in home

--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from re import template
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from salida.models import Salida
+
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['salidas'] = Salida.objects.all()
+        return context
+    
